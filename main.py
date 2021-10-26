bc66905c5aea1fefafda0ca406fc54515e0c34b5
keita0805carp <b181623@planet.kanazawa-it.ac.jp>
2021-10-26 11:45:22 +0900

    refactoring and add stastics debug

--- main.py
+++ main.py
@@ -10,18 +10,24 @@ PATH = '/var/log/auth.log'
 TOKEN = "CHANGEME"  # mapbox API Key
 SECRET = "abcdefg123456789"  # (Option)LogsViz Secret
 ##########################
+
+app = Flask(__name__)
 ips = {}
 ipv6_regex = r'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
 ipv4_regex = r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
 
 
 def parse(num):
-    id = 0
-    parsedJson = [{"count": id}]
+    app.logger.info("API is Called. Parsing...")
+    cntTotal = 0
+    cntKnownIP = 0
+    cntNewIP = 0
+    cntNullIP = 0
 
+    parsedJson = [{"count": cntTotal}]
     with open(PATH) as f:
         for line in (f.readlines()[num*-1:]):
-            id += 1
+            cntTotal += 1
             list_tmp = line.split()
 
             tmp = ' '.join(list_tmp[:3])
@@ -33,9 +39,11 @@ def parse(num):
             ip = checkIP(description)
 
             if ip != "" and ip in ips:
+                cntKnownIP += 1
                 longitude = ips[ip][0]
                 latitude = ips[ip][1]
             elif ip != "" and ip not in ips:
+                cntNewIP += 1
                 time.sleep(1)
                 url = "http://ip-api.com/json/"
                 url = str(url) + str(ip)
@@ -45,6 +53,7 @@ def parse(num):
                 latitude = str(jsonData['lat'])
                 ips[ip] = [longitude, latitude]
             else:
+                cntNullIP += 1
                 latitude = ""
                 longitude = ""
 
@@ -56,7 +65,7 @@ def parse(num):
 
             parsedJson_tmp = [
                     {
-                        'id': id,
+                        'id': cntTotal,
                         'Date': date,
                         'Time': ttime,
                         'target': target,
@@ -68,7 +77,11 @@ def parse(num):
                         }
                     ]
             parsedJson += parsedJson_tmp
-    parsedJson[0]["count"] = id
+    parsedJson[0]["count"] = cntTotal
+    app.logger.info("Total    : " + str(cntTotal))
+    app.logger.info("Known IPs: " + str(cntKnownIP))
+    app.logger.info("New IPs  : " + str(cntNewIP))
+    app.logger.info("Null IPs : " + str(cntNullIP))
     return parsedJson
 
 
@@ -86,10 +99,9 @@ def checkIP(description):
         return ip
 
 
-api = Flask(__name__)
 
 
-@api.route('/api/<string:key>', methods=["GET"])
+@app.route('/api/<string:key>', methods=["GET"])
 def ret_json(key):
     num = request.args.get('n', "100")
     if key == SECRET:
@@ -99,11 +111,11 @@ def ret_json(key):
     return make_response(jsonify(res))
 
 
-@api.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
     param = request.args.get('n', "100")
     return render_template("index.html", param=param, token=TOKEN, secret=SECRET)
 
 
 if __name__ == "__main__":
-    api.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
